802d615dd482ee143421687edb603808b8a22d61
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2024-04-01 16:43:50 -0500

    add methods in GameController

--- src/main/java/com/example/hangedman/controller/GameController.java
+++ src/main/java/com/example/hangedman/controller/GameController.java
@@ -7,6 +7,7 @@ import com.example.hangedman.view.WelcomeStage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -14,6 +15,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class GameController {
 
@@ -35,6 +37,9 @@ public class GameController {
 
     int j = 0;
 
+    String[] word;
+
+
     void setSecretWordContainer() {
         for (int i = 0; i < secretWord.getWord().length(); i++) {
             TextField wordTxt = new TextField();
@@ -45,6 +50,7 @@ public class GameController {
 
     @FXML
     void OnHandleButtonValidateLetter(ActionEvent event) {
+        boolean secretWordValidation = false;
         String PATH = "/com/example/hangedman/images/hangman/";
         String letter = letterField.getText().toLowerCase();
         boolean found = false;
@@ -55,6 +61,7 @@ public class GameController {
                     TextField wordTxt = (TextField) secretWordContainer.getChildren().get(i);
                     wordTxt.setText(letter);
                     found = true;
+                    word[i]= letter;
                 }
             }
             if (!found) {
@@ -64,10 +71,14 @@ public class GameController {
                 hangmanStatus.setImage(hangman);
                 resultTextArea.setText("La letra " + letter + " no está en la palabra secreta");
                 j++;
-//                if (j==5){
-//                    //Logica del codigo para que muestre un AlertBox y redirija a la pantalla de inicio
-//
-//                }
+                if (j==6){
+                    gameStatusAlert("Has perdido", "Lamentablemente no has adivinado la palabra. "+
+                    "Tu palabra era "+secretWord.getWord());
+                }
+            }
+
+            if (Arrays.equals(word, secretWord.getArrayWord())){
+                gameStatusAlert("Has ganado", "Enhorabuena! Has adivinado la palabra");
             }
         }
         else if ( !isValidWord(letter) || letter.length()!=1){
@@ -84,8 +95,28 @@ public class GameController {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    private void gameStatusAlert(String title, String message){
+        Alert gameStatusAlert = new Alert(Alert.AlertType.INFORMATION);
+        gameStatusAlert.setTitle(title);
+        gameStatusAlert.setHeaderText(null);
+        gameStatusAlert.setContentText(message);
+        gameStatusAlert.getButtonTypes().setAll(ButtonType.OK);
+        gameStatusAlert.setOnCloseRequest(eventOver -> {
+            GameStage.deleteInstance();
+            try {
+                WelcomeStage.getInstance();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        gameStatusAlert.showAndWait();
+    }
+
     public void setSecretWord(SecretWord secretWord) {
         this.secretWord = secretWord;
+        word = new String[secretWord.getArrayWord().length];
+
     }
     @FXML
     void OnHandleButtonBackTo(ActionEvent event) throws IOException {
@@ -109,7 +140,6 @@ public class GameController {
     }
 
     private boolean isValidWord(String word) {
-
         return word.matches("[a-zA-Z]+");
     }
 }
